cae631430cfd3457c30d268456281d1120766d39
Blair Perry <blair.perry@mavs.uta.edu>
2021-08-05 08:41:16 -0500

    initial work on circlize plotting script

--- analysis/4_promoters/PromoterH3K4me3_Workbook_08.04.21.R
+++ analysis/4_promoters/PromoterH3K4me3_Workbook_08.04.21.R
@@ -4,11 +4,9 @@ library(ggsignif)
 library(ggbeeswarm)
 library(patchwork)
 
-setwd("~/Dropbox/CastoeLabFolder/projects/CVV_Gene_Regulation/_VenomGeneRegulation/analyses/promoter_chipseq")
+ovrlp <- read_tsv('./analysis/4_promoters/PromoterH3K4me3_Overlap_12.08.20.bed',col_names = F)
 
-ovrlp <- read_tsv('PromoterH3K4me3_Overlap_12.08.20.bed',col_names = F)
-
-all.prom <- read_tsv('input_files/CvvGeneCoords_v2.bed',col_names = F)
+all.prom <- read_tsv('./analysis/4_promoters/input_files/CvvGeneCoords_v2.bed',col_names = F)
 
 no.ovrlp <- all.prom %>% 
   mutate(txid = str_remove_all(X4,'ID=')) %>% 
@@ -18,96 +16,102 @@ no.ovrlp <- all.prom %>%
 length(ovrlp$X1)
 length(ovrlp$X1) / length(all.prom$X1)
 
-exp <- read_csv('../../data/rnaseq/norm_counts/cvv_VenomRNAseq_NormCounts_updatedGFF_FullVenomAnnot_11.19.19.csv') %>% 
-  mutate(avg1DPE = rowMeans(.[,2:4])) %>% 
+exp <- read_csv('./analysis/1_gene_expression/norm_counts/CvvVenomReg_RepRNAseq_wNonVen_VSTNormCounts_08.02.21.csv') %>% 
+  mutate(avg1DPE = rowMeans(.[,c(4,8,9)])) %>% 
   mutate(geneID = str_split_fixed(X1,'_',2)[,1]) %>% 
   mutate(txID = str_split_fixed(X1,'_',2)[,2]) %>% 
   mutate(ChIPsignal = ifelse(txID %in% ovrlp$X4,'TRUE','FALSE')) %>% 
   mutate(ChIPsignal = factor(ChIPsignal,levels = c('TRUE','FALSE')))
 
-venPriorityGenes <- read_tsv('../../data/venom_annotations/___PriorityVenomGenes_Sept2020/PriorityVenomGeneList_withNVPs_02.25.21.txt',col_names = F) %>% 
+venPriorityGenes <- read_tsv('./data/venom_annotation/PriorityVenomGenes_08.02.21.txt',col_names = F) %>% 
   select(txid = 6)
 
 
-allGenes <- ggplot(exp,aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+allGenes <- ggplot(exp,aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0,vjust = 0.5 ) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
   ggtitle('d) All Genes') +
-  scale_y_continuous(limits=c(0,max(log10(exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(exp$avg1DPE+1)*1.2))) +
   xlab('H3K4me3 Peak Near Promoter') +
+  ylab('VST Normalized Gene Expression')+
   theme_bw() + theme(plot.title.position = 'plot',plot.title = element_text(face='bold'))
+
+allGenes
 
 venom.exp <- exp %>% 
   filter(exp$txID %in% venPriorityGenes$txid)
 
-allVen <- ggplot(venom.exp,aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+allVen <- ggplot(venom.exp,aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0,vjust = 0.5 ) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
   ggtitle('e) All Venom Genes') +
   xlab('H3K4me3 Peak Near Promoter') +
   theme_bw() + theme(axis.title.y = element_blank(),plot.title.position = 'plot',plot.title = element_text(face='bold'))
 
+allVen
 
-svmp <- ggplot(subset(venom.exp,str_detect(txID,'SVMP')),aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+svmp <- ggplot(subset(venom.exp,str_detect(txID,'SVMP')),aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_jitter(width = 0.1,size=1,show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0 ,vjust = 0.5) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
   ggtitle('f) SVMPs') +
   xlab('H3K4me3 Peak Near Promoter') +
+  ylab('VST Normalized Gene Expression')+
   theme_bw() + theme(plot.title.position = 'plot',plot.title = element_text(face='bold'))
 
-svsp <- ggplot(subset(venom.exp,str_detect(txID,'SVSP')),aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+svsp <- ggplot(subset(venom.exp,str_detect(txID,'SVSP')),aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_jitter(width = 0.1,size=1,show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0 ,vjust = 0.5) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
   ggtitle('g) SVSPs') +
   xlab('H3K4me3 Peak Near Promoter') +
   theme_bw() + theme(axis.title.y = element_blank(),plot.title.position = 'plot',plot.title = element_text(face='bold'))
 
 svsp
 
-svsp.2removed <- ggplot(venom.exp %>% filter(str_detect(txID,'SVSP') & txID != 'crovir-transcript-SVSP_2'),aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
-  geom_boxplot(show.legend = F) +
-  geom_jitter(width = 0.1,size=1,show.legend = F) +
-  geom_signif(comparisons = list(c("FALSE", "TRUE")),
-              map_signif_level = TRUE, textsize=4, tip_length = 0 ,vjust = 0.5) +
-  scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
-  ggtitle('g) SVSPs') +
-  xlab('H3K4me3 Peak Near Promoter') +
-  theme_bw() + theme(axis.title.y = element_blank(),plot.title.position = 'plot',plot.title = element_text(face='bold'))
+# svsp.2removed <- ggplot(venom.exp %>% filter(str_detect(txID,'SVSP') & txID != 'crovir-transcript-SVSP_2'),aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
+#   geom_boxplot(show.legend = F) +
+#   geom_jitter(width = 0.1,size=1,show.legend = F) +
+#   geom_signif(comparisons = list(c("FALSE", "TRUE")),
+#               map_signif_level = TRUE, textsize=4, tip_length = 0 ,vjust = 0.5) +
+#   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
+#   scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+#   ggtitle('g) SVSPs') +
+#   xlab('H3K4me3 Peak Near Promoter') +
+#   theme_bw() + theme(axis.title.y = element_blank(),plot.title.position = 'plot',plot.title = element_text(face='bold'))
+# 
+# svsp.2removed
 
-svsp.2removed
-
-pla2 <- ggplot(subset(venom.exp,str_detect(txID,'PLA2') & txID != 'crovir-transcript-PLA2_gIIE'),aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+pla2 <- ggplot(subset(venom.exp,str_detect(txID,'PLA2') & txID != 'crovir-transcript-PLA2_gIIE'),aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_jitter(width = 0.1,size=1,show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0 ,vjust = .5) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
   ggtitle('h) PLA2s') +
   xlab('H3K4me3 Peak Near Promoter') +
+  ylab('VST Normalized Gene Expression')+
   theme_bw() + theme(plot.title.position = 'plot',plot.title = element_text(face='bold'))
 
-other <- ggplot(subset(venom.exp,str_detect(txID,'SVSP|SVSP|PLA2|128',negate = T)),aes(x=ChIPsignal,y=log10(avg1DPE+1),fill=ChIPsignal)) +
+other <- ggplot(subset(venom.exp,str_detect(txID,'SVSP|SVSP|PLA2|128',negate = T)),aes(x=ChIPsignal,y=avg1DPE,fill=ChIPsignal)) +
   geom_boxplot(show.legend = F) +
   geom_jitter(width = 0.1,size=1,show.legend = F) +
   geom_signif(comparisons = list(c("FALSE", "TRUE")),
               map_signif_level = TRUE, textsize=4, tip_length = 0,vjust = 0.5) +
   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-  scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
+  # scale_y_continuous(limits=c(0,max(log10(venom.exp$avg1DPE+1)*1.2))) +
   xlab('H3K4me3 Peak Near Promoter') +
   ggtitle('i) Other VGs') +
   theme_bw() + theme(axis.title.y = element_blank(),plot.title.position = 'plot',plot.title = element_text(face='bold'))
@@ -119,23 +123,3 @@ other <- ggplot(subset(venom.exp,str_detect(txID,'SVSP|SVSP|PLA2|128',negate = T
 
 (allGenes | allVen) / (svmp | svsp ) / (pla2 | other) 
 
-# ### Overlap with promoters near ATACseq peak only in VG3
-# 
-# atac.ovrlp <- read_tsv('./z_old/promoters_H3K4me3_overlap_genomeWide_extended_1000bp_VG3_not_in_VGU.bed',col_names = F) %>% 
-#   mutate(X1 = str_remove_all(X1,'ID='))
-# 
-# exp.atac <- exp %>% 
-#   mutate(atac_overlap = ifelse(txID %in% atac.ovrlp$X1,T,F)) %>% 
-#   mutate(both_overlap = ifelse(atac_overlap == T & ChIPsignal == T,T,NA)) %>% 
-#   mutate(both_overlap = ifelse(atac_overlap == F & ChIPsignal== F,F,both_overlap))
-# 
-# ggplot(subset(exp.atac,!is.na(both_overlap)),aes(x=both_overlap,y=log10(avg1DPE+1),color=both_overlap)) +
-#   geom_jitter(alpha=0.25,size=0.3) +
-#   geom_boxplot(show.legend = F,color='black',fill='NA') +
-#   # geom_quasirandom(alpha=0.4) +
-#   geom_signif(comparisons = list(c("FALSE", "TRUE")),
-#               map_signif_level = TRUE, textsize=4, tip_length = 0,vjust = 0.5 ) +
-#   scale_fill_manual(values=c('FALSE'='grey','TRUE'='skyblue2')) +
-#   ggtitle('All Genes') +
-#   xlab('H3K4me3 Peak Near Promoter & Unique ATAC-seq peak at VG3') +
-#   theme_bw()
